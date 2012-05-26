3d1a76ac30ceff67116a0325b9c0c7cac1d9ef4a
Andre G. da Silva <andre2205p@yahoo.com.br>
2012-05-26 09:57:02 -0300

    incluido arquivo main

---
+++ Notpad/main.cpp
@@ -0,0 +1,26 @@
+#include <QApplication>
+#include <QLabel>
+#include <QPushButton>
+#include <QTextEdit>
+#include <QVBoxLayout>
+#include "not_pad.h"
+#include "welcome_window.h"
+
+int main(int argc, char **argv)
+{
+    QApplication app(argc, argv);
+
+    //Notpad note;
+
+    Not_pad note(&app);
+    note.show();
+    note.setarTexto("setarTexto");
+
+    Welcome_Windows wwin;
+    wwin.setText("Ok");
+
+    wwin.show();
+    QObject :: connect (&wwin,SIGNAL(clicked()),&note,SLOT(exibirTexto()));
+
+    return app.exec();
+}
